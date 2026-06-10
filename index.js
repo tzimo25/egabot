e73e073d628c676edfff4bf7cb64f9a482025337
tzimo25 <tzimo2000@gmail.com>
2026-06-10 15:50:09 +0300

    Refactor index.js to log bot status on ready

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
+
+console.log("TOKEN =", process.env.TOKEN);
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+  console.log("Bot is online");
 });
 
 client.login(process.env.TOKEN);
